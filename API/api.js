798b996296087849ba1beb7041e71a665d977792
Deltalisk <tedgepublic@gmail.com>
2020-05-19 12:50:23 -0700

    Unit testing is now confirmed
    We are now bootstrapped into more serious testing

--- API/api.js
+++ API/api.js
@@ -46,16 +46,16 @@ router.use(function (req,res,next) {
 // you can test GETs with a browser using URL http://localhost:3000/api/restaurants or http://localhost:3000/api/restaurants/30075445
 // recommend Postman app for testing other verbs, find it at https://www.postman.com/
 router.get("/",function(req,res){
-	res.send("Yo!  This my API.  Call it right, or don't call it at all!");
+	res.send("Hello, you've reached my API without calling anything. Sup?");
 });
 
 // NEW_USER - receive new user registration data
-router.get("/api/users/new", function(req, res){
-	global.connection.query() {		//TODO: finish query
-		if(error) res.send();		//TODO: finish error msg
-		else res.send(JSON.stringify({"status": 200, "error": null, "response": results}) //TODO: does this need to be modified?
-	} 
-})
+// router.get("/api/users/new", function(req, res){
+// 	global.connection.query() {		//TODO: finish query
+// 		if(error) res.send();		//TODO: finish error msg
+// 		else res.send(JSON.stringify({"status": 200, "error": null, "response": results}) //TODO: does this need to be modified?
+// 	} 
+// })
 
 // GET - read data from database, return status code 200 if successful
 router.get("/api/restaurants",function(req,res){
@@ -77,10 +77,10 @@ router.get("/api/users/:id",function(req,res){
 
 
 
-// start server running on port 3000 (or whatever is set in env)
+// start server running on port 3306 (or whatever is set in env)
 app.use(express.static(__dirname + '/'));
 app.use("/",router);
-app.set( 'port', ( process.env.PORT || config.port || 3000 ));
+app.set( 'port', ( process.env.PORT || config.port || 3306 ));
 
 app.listen(app.get( 'port' ), function() {
 	console.log( 'Node server is running on port ' + app.get( 'port' ));
--- Testing/api_unit_test.py
+++ Testing/api_unit_test.py
@@ -10,74 +10,86 @@ Requires installation of mysql connector: pip install mysql-connector-python
 Based on: https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
 '''
 
-DEFAULT_TGT_URL= "x" #TODO: What's our freaking directory?
+DEFAULT_TGT_URL= "http://localhost:3306"
 
 
-def make_get_call(url, data):
-    # make get call to url
-    resp = requests.get(url, json= data)
-    # expecting to get a status of 200 on success
-    if resp.json()['status'] != 200:
-        # This means something went wrong.
-        print('Something went wrong {}'.format(resp.status_code))
-        exit()
-    # print data returned
-    print("get succeeded")
-    print("received: ")
-    print(resp.json())
-    # for employee in resp.json()['response']:
-    #     print(employee["EmployeeID"], employee["EmployeeLastname"], employee["AdminPrivileges"])
-
-def make_post_call(url, data):
-    # make post call to url passing it data
-    resp = requests.post(url, json=data)
-    # expecting to get a status of 201 on success
-    if resp.json()['status'] != 201:
-        print('Something went wrong {}'.format(resp.status_code))
-        exit()
-    print('post succeeded')
-    print("received: ")
-    print(resp.json())
-
-
-def make_put_call(url, data):
-    # make post call to url passing it data
-    # structure this as such: "EmployeeFirstName= 'Bill'"
-    # one field at a time
-    resp = requests.put(url, json=data)
-    # expecting to get a status of 200 on success
-    if resp.json()['status'] != 200:
-        print('Something went wrong {}'.format(resp.status_code))
-        exit()
-    print('put succeeded')
-    print("received: ")
-    print(resp.json())
-
-
-def make_delete_call(url):
-    # make post call to url passing it data
-    resp = requests.delete(url)
-    # expecting to get a status of 200 on success
-    if resp.json()['status'] != 200:
-        print('Something went wrong {}'.format(resp.status_code))
-        exit()
-    print('delete succeeded')
-    print("received: ")
-    print(resp.json())
+# def make_get_call(url, data):
+#     # make get call to url
+#     resp = requests.get(url, json= data)
+#     # expecting to get a status of 200 on success
+#     if resp.json()['status'] != 200:
+#         # This means something went wrong.
+#         print('Something went wrong {}'.format(resp.status_code))
+#         exit()
+#     # print data returned
+#     print("get succeeded")
+#     print("received: ")
+#     print(resp.json())
+#     # for employee in resp.json()['response']:
+#     #     print(employee["EmployeeID"], employee["EmployeeLastname"], employee["AdminPrivileges"])
+#
+# def make_post_call(url, data):
+#     # make post call to url passing it data
+#     resp = requests.post(url, json=data)
+#     # expecting to get a status of 201 on success
+#     if resp.json()['status'] != 201:
+#         print('Something went wrong {}'.format(resp.status_code))
+#         exit()
+#     print('post succeeded')
+#     print("received: ")
+#     print(resp.json())
+#
+#
+# def make_put_call(url, data):
+#     # make post call to url passing it data
+#     # structure this as such: "EmployeeFirstName= 'Bill'"
+#     # one field at a time
+#     resp = requests.put(url, json=data)
+#     # expecting to get a status of 200 on success
+#     if resp.json()['status'] != 200:
+#         print('Something went wrong {}'.format(resp.status_code))
+#         exit()
+#     print('put succeeded')
+#     print("received: ")
+#     print(resp.json())
+#
+#
+# def make_delete_call(url):
+#     # make post call to url passing it data
+#     resp = requests.delete(url)
+#     # expecting to get a status of 200 on success
+#     if resp.json()['status'] != 200:
+#         print('Something went wrong {}'.format(resp.status_code))
+#         exit()
+#     print('delete succeeded')
+#     print("received: ")
+#     print(resp.json())
 
 def create_dummy_users(url):
     resp= requests.put(url+"/users/new", )
 
-def testSequence():
-    print("Here is where I would test the code... if I HAD any!")
+def test_api_running(url):
+    print("\ttesting local connection")
+    resp= requests.get(url)
+    # print("\t\t"+resp.text)
+    if resp.text== "Hello, you've reached my API without calling anything. Sup?":
+        print("\tlocal api is running")
+        return True
+    else:
+        print("\tapi not found!")
+        return False
 
+def testSequence(url):
+    print("Beginning test sequence")
+    if not test_api_running(url):
+        exit(2)
 
 if __name__ == '__main__':
 
     tgtURL= input("enter target URL or use default:")
-    if tgtURL= "":
+    if tgtURL== "":
         tgtURL= DEFAULT_TGT_URL
-    testSequence()
+    testSequence(tgtURL)
 
 
 
