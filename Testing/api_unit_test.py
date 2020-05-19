b0a923c79ba15c27cb0e769b3e6f0ca4d90e881e
Deltalisk <tedgepublic@gmail.com>
2020-05-18 21:23:29 -0700

    Updated api_unit_test.py
    Still don't run, but is being improved

--- Testing/api_unit_test.py
+++ Testing/api_unit_test.py
@@ -2,14 +2,12 @@ import requests
 import json
 
 '''
-Client side demo to fetch data from a RESTful API.  Assumes Node.js file api is running (nodemon api.js <localhost|sunapee>) 
-on the server side.
-Author: Tim Pierson, Dartmouth CS61, Spring 2020
+Unit Testing for grocery-tracker project
+Author: Ted G, Dartmouth CS61, Spring 2020
+Group 14: Ted G, Nina P, Rodrigo CB, Anders L
 Requires installation of mysql connector: pip install mysql-connector-python
 	also requires Requests: pip install requests
 Based on: https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
-
-Usage: python call_api.py 
 '''
 
 
@@ -65,50 +63,6 @@ def make_delete_call(url):
     print("received: ")
     print(resp.json())
 
-def handleInput(usr, pwd):
-    print("get, post, put, or delete query?\ntype '/' to cancel operation")
-    actionType= input()
-    if actionType[0] == "/":
-        if actionType== "/quit":
-            return False
-    elif actionType == "get":
-        print("\tgetting. Enter subject ID or press return for your own:")
-        tgtID= input()
-        if tgtID[0] == '/':
-            pass
-        elif tgtID== "":
-            data = {"username": usr, "pwd": pwd}
-            make_get_call('http://localhost:3000/api/employees/', data)
-        else:
-            data = {"username": usr, "pwd": pwd, "EmployeeID": tgtID}
-            make_get_call('http://localhost:3000/api/employees/'+tgtID, data)
-    elif actionType == "post":
-        print("\tposting. Type the modification you wish to make")
-        print("\tfirst name?")
-        firstname= input()
-        print("\tlast name?")
-        lastname= input()
-        username= firstname.lower()+lastname.lower()
-        print("\tpassword name?")
-        password= input()
-        data = {"username": usr, "pwd": pwd, "EmployeeFirstname": firstname, "Lastname":lastname,
-                "EmployeeUsrname": username, "EmployeeHashword":password}
-        make_post_call('http://localhost:3000/api/employees/', data)
-    elif actionType == "put":
-        print("\tputting. target ID?")
-        tgtID= input()
-        print("\tattribute to modify?")
-        tgtAttrib= input()
-        print("\tnew value?")
-        tgtVal= input()
-        data = {"username": usr, "pwd": pwd, tgtAttrib: tgtVal, "EmployeeID": tgtID}
-        make_put_call('http://localhost:3000/api/employees/'+tgtID, data)
-    elif actionType == "delete":
-        print("\tdeleting. Type ID to delete")
-        tgtID= input()
-        data = {"username": usr, "pwd": pwd, "EmployeeID": tgtID}
-        make_delete_call('http://localhost:3000/api/employees/'+tgtID, data)
-
 def testSequence():
     # testing get calls
     print("Making a get (read) call to employees")
@@ -142,32 +96,7 @@ def testSequence():
 
 if __name__ == '__main__':
 
-    keepGoing = True
-    loggedIn = False
-    username = ""
-    password = ""
-    while keepGoing:
-        if loggedIn:
-            print("logged in as "+ username)
-            if not handleInput(username, password):
-                print("shutting down")
-                quit(0)
-        else:
-            print("type username and password to log in") # login saves credentials for use in each query
-            print("type /quit to quit or /test to test automatically")
-            print("username:")
-            username = input()
-            if username[0] == "/":
-                if username == "/quit":
-                    print("shutting down")
-                    quit(0)
-                if username == "/test":
-                    print("performing test sequence")
-                    testSequence()
-            else:
-                print("password:")
-                password = input()
-                loggedIn = True
+    testSequence()
 
 
 
